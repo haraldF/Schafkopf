c07f9bbd26f854b2576920873829e2b76e171260
Harald Fernengel <harald.fernengel@here.com>
2015-09-10 20:43:36 +0300

    more testing

--- test.cpp
+++ test.cpp
@@ -4,37 +4,76 @@
 
 using namespace SchafKopf;
 
-TEST(TestSchafKopf, stiche)
+
+static void testStiche(Game &game, Card card1, Card card2, Card card3, Card card4, int winner)
+{
+    game.activePile.put(card1);
+    game.activePile.put(card2);
+    game.activePile.put(card3);
+    game.activePile.put(card4);
+    game.doStich();
+    ASSERT_EQ(winner, game.m_lastStichPlayer);
+}
+
+static void testStiche(Card card1, Card card2, Card card3, Card card4, int winner)
 {
     Game game;
     game.gameType = Game::Solo;
     game.gameColor = Color::Herz;
 
-    game.activePile.put(Card{Ass, Schelln});
-    game.activePile.put(Card{Zehner, Schelln});
-    game.activePile.put(Card{Unter, Schelln});
-    game.activePile.put(Card{Siebner, Schelln});
-    game.doStich();
-    ASSERT_EQ(2, game.m_lastStichPlayer);
+    ASSERT_NO_FATAL_FAILURE(testStiche(game, card1, card2, card3, card4, winner));
+}
 
-    game.activePile.put(Card{Zehner, Schelln});
-    game.activePile.put(Card{Ass, Schelln});
-    game.activePile.put(Card{Achter, Schelln});
-    game.activePile.put(Card{Siebner, Schelln});
-    game.doStich();
-    ASSERT_EQ(1, game.m_lastStichPlayer);
+TEST(TestSchafKopf, stiche)
+{
+    ASSERT_NO_FATAL_FAILURE(
+        testStiche(Card{Ass, Schelln},
+                   Card{Zehner, Schelln},
+                   Card{Unter, Schelln},
+                   Card{Siebner, Schelln},
+                   2));
 
-    game.activePile.put(Card{Ober, Schelln});
-    game.activePile.put(Card{Unter, Schelln});
-    game.activePile.put(Card{Ober, Eichel});
-    game.activePile.put(Card{Siebner, Schelln});
-    game.doStich();
-    ASSERT_EQ(2, game.m_lastStichPlayer);
+    ASSERT_NO_FATAL_FAILURE(
+        testStiche(Card{Zehner, Schelln},
+                   Card{Ass, Schelln},
+                   Card{Achter, Schelln},
+                   Card{Siebner, Schelln},
+                   1));
 
-    game.activePile.put(Card{Achter, Schelln});
-    game.activePile.put(Card{Ass, Gras});
-    game.activePile.put(Card{Ass, Eichel});
-    game.activePile.put(Card{Siebner, Schelln});
-    game.doStich();
-    ASSERT_EQ(0, game.m_lastStichPlayer);
+    ASSERT_NO_FATAL_FAILURE(
+        testStiche(Card{Ober, Schelln},
+                   Card{Unter, Schelln},
+                   Card{Ober, Eichel},
+                   Card{Siebner, Schelln},
+                   2));
+
+    ASSERT_NO_FATAL_FAILURE(
+        testStiche(Card{Achter, Schelln},
+                   Card{Ass, Gras},
+                   Card{Ass, Eichel},
+                   Card{Siebner, Schelln},
+                   0));
+}
+
+TEST(TestSchafKopf, sticheInGame)
+{
+    Game game;
+    game.gameType = Game::Solo;
+    game.gameColor = Color::Herz;
+
+    ASSERT_NO_FATAL_FAILURE(
+        testStiche(game,
+                   Card{Ass, Schelln},
+                   Card{Zehner, Schelln},
+                   Card{Unter, Schelln},
+                   Card{Siebner, Schelln},
+                   2));
+
+    ASSERT_NO_FATAL_FAILURE(
+        testStiche(game,
+                   Card{Zehner, Gras},
+                   Card{Ass, Schelln},
+                   Card{Achter, Schelln},
+                   Card{Siebner, Schelln},
+                   1));
 }
